79674d416fc62112f05baf6d8173ec8b142d3fd5
BuildTools <daniel@leining.org>
2018-06-13 15:55:32 -0400

    ensure background worker stops

--- background_worker.go
+++ background_worker.go
@@ -19,13 +19,14 @@ func (bw *BackgroundWorker) backgroundWorkerFunc() {
 		select {
 		case <-bw.quitChan:
 			log.Println("stopping worker")
+			bw.running = false
+			return
 		default:
 			log.Printf("Sleeping for %s", bw.c.config.WorkerSleepTime)
 			time.Sleep(bw.c.config.WorkerSleepTime)
 			bw.c.ExecuteOneJob()
 		}
 	}
-	bw.running = false
 }
 
 func (bw *BackgroundWorker) Start() {
